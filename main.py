4b082ff054cf64a1cb76de67e90c9a005fb1d60a
Axw_0 <qianyucun07@163.com>
2025-01-20 07:10:29 +0800

    upload

--- main.py
+++ main.py
@@ -1,35 +1,24 @@
 import os
 import re
-from idlelib.pyparse import trans
+import subprocess
 
-location1 = "D:\Desktop\WTTRtool\langt"
-location2 = "lang"
-location3 = "lang_test"
+key_pattern = re.compile(';\n"([a-zA-Z0-9_\n]+(?:/[a-zA-Z0-9_\n]+)+)"')
 
-
-# 死妈BVVD写的逆天翻译文件让我用了4个小时写读取
 
 def read_f(f):
-    # pattern = re.compile(';\n((?:[a-zA-Z0-9_\n]+/[a-zA-Z0-9_\n]+)+;)')
-    pattern = re.compile('\n"([a-zA-Z0-9_\n]+(?:/[a-zA-Z0-9_\n]+)+)"')
-    tr_key = []
     tr_array = []
     rows = []
     with open(f, 'r', encoding='utf-8') as file:
         try:
-            f_row = str(file.readline()).split(";")
+            f_row = str(file.readline()).split('\";\"')
             cn_loc = f_row.index('"<Chinese>"')
         except ValueError:
             return
         reader = file.readlines()
-        # reader = ''.join(reader).replace('"', '').replace('\'', '') \
-        #     .replace('\\t', '').replace('\\', '')
-        # reader = ';\n' + reader
     reader = ''.join(reader)
-    reader = '\n' + reader
-    tr_key = pattern.findall(reader)
-    print(tr_key)
-    reader = re.split(pattern, reader)
+    reader = ';;\n' + reader
+    tr_key = key_pattern.findall(reader)
+    reader = re.split(key_pattern, reader)
     for i in range(len(reader)):
         if i % 2 == 1:
             rows.append(reader[i] + reader[i + 1])
@@ -41,7 +30,7 @@ def read_f(f):
 
 def export(path, original, fname="output"):
     with open(f"{fname}.atrf", "wt", encoding='utf-8') as atr:
-        atr.write("[WTTRtool Axw Translate File v1.0]\n")
+        atr.write("[WTTRtool Translate File v1.0]\n")
     files = [entry for entry in os.listdir(path) if
              os.path.isfile(os.path.join(path, entry)) and entry.endswith(".csv")]
     for f in files:
@@ -64,22 +53,54 @@ def export(path, original, fname="output"):
 
 
 def write_f(f, d):
-    with open(f, "r", encoding='utf-8') as f:
-        f_row = str(f.readline()).split(";")
-        cn_loc = f_row.index('"<Chinese>"')
-        r_data = f.readlines()
-    for line in range(len(r_data)):
-        r_data[line] = re.findall("\"[^;]*\"", r_data[line])
-        print(r_data[line])
+    rows = []
+    with open(f, "r", encoding='utf-8') as file:
+        f_row = str(file.readline()).split(';')
+        try:
+            cn_loc = f_row.index('"<Chinese>"')
+        except ValueError:
+            return
+        reader = file.readlines()
+
+    reader = ''.join(reader)
+    reader = ';;\n' + reader
+    tr_key = key_pattern.findall(reader)
+    tr_row = dict.fromkeys(tr_key)
+    reader = re.split(key_pattern, reader)
+    for i in range(len(reader)):
+        if i % 2 == 1:
+            rows.append(reader[i] + '"' + reader[i + 1])
+    for row in rows:
+        if re.search(r"\"[;\d]{2,}", row):
+            t_row = re.split(key_pattern, ";\n\"" + row)[1:]
+            tr_row[t_row[0]] = t_row[1][1:]
+        else:
+            t_row = row.split("\";\"")
+            t_row[cn_loc] = t_row[cn_loc].replace("\n", "\\n").replace('\\t', '')
+            tr_row[t_row[0]] = t_row[1:]
+
     for trans in d:
-        quit()
+        t_trans = trans[2]
+        for index in range(len(trans[2]) - 1, -1, -1):
+            # if re.match(r"[^\x00-\xff]{2}", trans[2][index:index+2]):  # 全角匹配
+            if re.match(r"[\u4e00-\u9fa5]{2}", trans[2][index:index + 2]):  # 汉字匹配
+                t_trans = t_trans[:index + 1] + '\\t' + t_trans[index + 1:]
+        if type(tr_row[trans[0]]) is not str:
+            tr_row[trans[0]][cn_loc - 1] = t_trans
+
+    data = ''
+    for key in tr_row.keys():
+        line = '";"'.join(tr_row[key])
+        data = f"{data}\n\"{key}\";\"{line};"
+    with open(f, "w", encoding='utf-8') as file:
+        file.write(data)
 
 
 def inport(f, path):
     with open(f, "r", encoding='utf-8') as atr:
         art = re.findall('(\[.*\.csv\])(\n[^\[]*)?', atr.read())
-    # print(art[0][1])
     for file in art:
+        print(file[0])
         this_file = file[0][1:-1]
         trans = file[1].strip('\n')
         trans = trans.split('\n')
@@ -93,14 +114,6 @@ def inport(f, path):
                 trans[line][2] = trans[line][2].strip('"')
         if not path.endswith('/'):
             path = path + '/'
-        write_f(path+this_file, trans)
+        write_f(path + this_file, trans)
     return
 
-
-export(location1, location2)
-# inport("output.atrf", location3)
-# test1 = "lang_test/benchmark.csv"
-test1 = "test.csv"
-test2 = [['controls/controlsDamaged', '飞行操控受损', 'Flight control damaged'], ['controls/weaponControlsDamaged', '航电设备损毁，无法操控武器', 'Avionics damaged, weapon countrol systems cannot be use'], ['controls/guidanceDamaged', '航电设备损毁，无法操控制导武器', 'Avionics damaged, guidance weapon countrol systems cannot be use'], ['controls/sensorsDamaged', '传感器受损', 'Sensors damaged'], ['controls/radarDamaged', '雷达受损', 'Radar damaged'], ['controls/rwrDamaged', '红外干扰系统受损', 'IRCM damaged'], ['controls/twsDamaged', '扫描跟踪损坏', 'TWS damaged'], ['controls/opticsDamaged', '瞄准设备受损', 'Targeting optic damaged'], ['controls/targetingPodDamaged', '制导吊舱受损', 'Targeting Pod damaged'], ['controls/countermeasuresDamaged', '红外干扰系统受损', 'IRCM damaged'], ['controls/nightVisionDamaged', '航电设备损毁，无法使用夜视系统', 'Avionics damaged, NVD systems cannot be use'], ['controls/indicatorsDamaged', '航电设备损毁，多功能显示器/平视显示器无法使用', 'Avionics damaged, MFD/HUD systems cannot be use']]
-# write_f(test1, test2)
-# print(read_f(test1))
