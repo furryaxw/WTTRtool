3511586e601b857f30eba15583708b45b50da13c
furryaxw <qianyucun07@163.com>
2025-01-21 04:19:04 +0800

    Update config system

---
+++ config.py
@@ -0,0 +1,49 @@
+import json
+
+
+class Config:
+    file: str = None
+    default: dict = None
+
+    def __init__(self, file: str, default: dict = None):
+        self.default = default
+        self.file = "./"+file+".json"
+        try:
+            with open(self.file, 'r') as f:
+                self.conf = json.load(f)
+        except Exception as e:
+            print("配置文件异常，正在重置")
+            print("错误代码：" + str(e))
+            self.data = json.dumps(default, indent=4)
+            with open(self.file, 'w') as f:
+                f.write("\n" + self.data)
+
+    def read(self):
+        try:
+            with open(self.file, 'r') as f:
+                self.conf = json.load(f)
+            return self.conf
+        except Exception as e:
+            print("读取配置文件异常")
+            print("错误代码：" + str(e))
+            return -1
+
+    def write(self, t: dict):
+        try:
+            self.conf.update(t)
+            with open(self.file, 'w') as f:
+                data = json.dumps(self.conf, indent=4)
+                f.write("\n" + data)
+                f.flush()
+        except Exception as e:
+            print("读取配置文件异常")
+            print("错误代码：" + str(e))
+            return -1
+
+    def wipe(self):
+        with open(self.file, 'w') as f:
+            f.write("\n" + self.data)
+            f.flush()
+        with open(self.file, 'r') as f:
+            self.conf = json.load(f)
+
